056e318bbdf49cecec0981803af8c7419acc4f42
ArijitChakr <arijit.chakraborty2304@gmail.com>
2025-01-11 17:55:46 +0530

    complete landing page

--- src/containers/blog/Blog.tsx
+++ src/containers/blog/Blog.tsx
@@ -11,14 +11,14 @@ const Blog = () => {
         </h2>
       </div>
       <div className="gpt3__blog-container">
-        <div className="gpt3__blog-container_groutA">
+        <div className="gpt3__blog-container_groupA">
           <Article
             imgURL={blog01}
             date="Sep 26, 2021"
             title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
           />
         </div>
-        <div className="gpt3__blog-container_groutA">
+        <div className="gpt3__blog-container_groupB">
           <Article
             imgURL={blog02}
             date="Sep 26, 2021"
--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -1,9 +1,65 @@
 import "./footer.css";
+import logo from "../../assets/GPT-3.svg";
+
+const FooterLinks = ({
+  heading,
+  links,
+}: {
+  heading: string;
+  links: string[];
+}) => {
+  return (
+    <>
+      <h4>{heading}</h4>
+      {links.map((link, index) => (
+        <p key={index}>{link}</p>
+      ))}
+    </>
+  );
+};
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>footer</p>
+    <footer className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h2 className="gradient__text">
+          Do you want to step in to the future before others
+        </h2>
+      </div>
+      <div className="gpt3__footer-button">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <FooterLinks
+            heading="Links"
+            links={["Overons", "Social Media", "Counters", "Contact"]}
+          />
+        </div>
+        <div className="gpt3__footer-links_div">
+          <FooterLinks
+            heading="company"
+            links={["Terms & Conditions", "Privacy Policy", "Contact"]}
+          />
+        </div>
+        <div className="gpt3__footer-links_div">
+          <FooterLinks
+            heading="Get in touch"
+            links={[
+              "Crechterwoord K12 182 DK Alknjkcb",
+              "085-132567",
+              "info@payme.net",
+            ]}
+          />
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>Copyright © 2021 GPT-3 All rights reserved</p>
+      </div>
     </footer>
   );
 };
